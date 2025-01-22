27fe5e34ba330c917a497687df630f54b9e63cc8
William Kim <wilkim1004@gmail.com>
2025-01-21 19:05:36 -0800

    Added getting users by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -49,6 +49,21 @@ app.get("/users", (req, res) => {
   }
 });
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
